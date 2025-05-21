4ebbe1272641a0d85ac84426fcec499959296801
MotiWolff <motiwolff@gmail.com>
2025-05-21 22:38:48 +0300

    Add strikeUnits

---
+++ src/StrikeUnits/F16FighterJet.cs
@@ -0,0 +1,35 @@
+using OperationFirstStrike.Core.Interfaces;
+using OperationFirstStrike.Core.Models;
+using OperationFirstStrike.Utils;
+
+namespace OperationFirstStrike
+{
+    public class F16FighterJet : IStrikeUnit
+    {
+        public string Name { get; set; }
+        public int Ammo { get; set; }
+        public int Fuel { get; set; }
+
+        public F16FighterJet(string name = "F-16 Fighter Jet", int initialAmmo = 6, int initialFuel = 100)
+        {
+            Name = name;
+            Ammo = initialAmmo;
+            Fuel = initialAmmo;
+        }
+        public bool CanStrike(string targetType)
+        {
+            return targetType == "Building" || targetType == "Vehicle";
+        }
+
+        public void PerformStrike(Terrorist target, IntelligenceMessage intel)
+        {
+            if (Ammo > 0 && Fuel >= 20)
+            {
+                target.IsAlive = false;
+                Ammo--;
+                Fuel -= 20;
+            }
+        }
+
+    }
+}
